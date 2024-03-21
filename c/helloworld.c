f169ec2f070e58ae179bee27c8adce69d3b0abac
coderun13 <snehalsingh.0407@gmail.com>
2024-03-22 01:19:21 +0530

    basic code

--- c/helloworld.c
+++ c/helloworld.c
@@ -4,5 +4,17 @@ int main()
 {
     printf("Hello World\n");
     return 0;
+
+    int a, b;
+    printf("Enter number a\n");
+    scanf("%d", &a);
+
+    printf("Enter number b\n");
+    scanf("%d", &b);
+
+    printf("The sum is %d\n", a+b);
+
+    return 0;
 }
+
 
