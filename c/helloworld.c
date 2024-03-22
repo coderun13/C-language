a84d72d671ad2aba62748266e43d4a1045d949b3
coderun13 <snehalsingh.0407@gmail.com>
2024-03-22 18:45:20 +0530

    hello world

--- c/helloworld.c
+++ c/helloworld.c
@@ -4,17 +4,6 @@ int main()
 {
     printf("Hello World\n");
     return 0;
-
-    int a, b;
-    printf("Enter number a\n");
-    scanf("%d", &a);
-
-    printf("Enter number b\n");
-    scanf("%d", &b);
-
-    printf("The sum is %d\n", a+b);
-
-    return 0;
 }
 
 
