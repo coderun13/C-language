4a2146815b4b840eb2332804f187f7b8a27c60f6
coderun13 <snehalsingh.0407@gmail.com>
2024-04-21 17:01:51 +0530

    hello world

--- c/helloworld.c
+++ c/helloworld.c
@@ -6,4 +6,22 @@ int main()
     return 0;
 }
 
+int main()
+{
 
+    printf("hello\n");
+
+    printf("snehal\n");
+
+    int v,r;
+
+    printf("enter value of r ",r);
+
+    scanf("%d",&r);
+
+    v=4*3*r*r*r/3;
+
+    printf("the value is volume is %d",v);
+
+    return 0;
+}
