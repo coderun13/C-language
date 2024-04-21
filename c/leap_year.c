6f3a449cb1a6899524d034e808b3d22f7ef570b2
coderun13 <snehalsingh.0407@gmail.com>
2024-04-21 16:56:36 +0530

    leap_year

---
+++ c/leap_year.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int a;
+
+    printf("enter a number ");
+
+    scanf("%d",&a);
+
+    if (a%4==0)
+
+    {
+
+        printf("this is a leap year");
+
+    }else{
+
+        printf("this is not a leap year");
+
+    }
+
+    }
