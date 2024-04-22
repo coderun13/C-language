c820cd896ef32572d683aeaed808974463015a9d
coderun13 <snehalsingh.0407@gmail.com>
2024-04-22 22:38:31 +0530

    number by index

--- c/arrays.c
+++ c/arrays.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
 int main()
+// {
+//     int arr[5]= {2,4,6,8,1};
+//     arr[4]= 100;
+//     arr[1]= 1;
+//     printf("%d",arr[1]);
+//     return 0;
+
+// }
+
 {
-    int arr[5]= {2,4,6,8,1};
-    arr[4]= 100;
-    arr[1]= 1;
-    printf("%d",arr[1]);
-    return 0;
+    int arr1[4];
+    printf("enter first number: ");
+    scanf("%d",&arr1[0]);
+     printf("enter second number: ");
+    scanf("%d",&arr1[1]);
+     printf("enter third number: ");
+    scanf("%d",&arr1[2]);
+     printf("enter fourth number: ");
+    scanf("%d",&arr1[3]);
+
+    printf("%d",arr1[3]);
 }
+
