70d3651a257a7cf77069ed6326e04ffa62ba575e
coderun13 <snehalsingh.0407@gmail.com>
2024-04-30 00:46:02 +0530

    string as pointer

--- c/strings.c
+++ c/strings.c
@@ -1,13 +1,17 @@
 //Strings - charcter arrays
 
-#include<stdio.h>
-/*int main(){
+/*
+ #include<stdio.h>
+    int main(){
     char arr[5] = {'a','b','c','d','e'};
     printf("%c",arr[2]);
     return 0;
-}*/
+}
+*/
 
-/*int main(){
+/* 
+#include<stdio.h>
+    int main(){
     char arr[] = {'h','e','l','l','o','!'};
     int i = 0;
      while(arr[i]!= '!'){
@@ -15,9 +19,12 @@
         i++;
      }
      return 0;
-};*/
+};
+*/
 
-/*int main(){
+/* 
+#include<stdio.h>
+    int main(){
     char arr[] = {"Hello Snehal\0"};
     int i = 0;
      while(arr[i]!= '\0'){
@@ -25,22 +32,50 @@
         i++;
      }
      return 0;
-};*/
+};
+*/
 
 
 //gets(), puts() (for string)
 //%s - format specifier for string
 
-#include<string.h>
-int main(){
-    //char str[] = "snehal singh";
+/* 
+  #include<stdio.h>
+  #include<string.h>
+    int main(){
+    char str[] = "snehal singh";
     char str[40];
 
-    //scanf("%s",str); //only the first word will be considered.
+    scanf("%s",str); //only the first word will be considered.
     gets(str); //entire sentence can be taken as input
     
     printf("the input was: %s",str);
-    //puts(str);
+    puts(str);
 
 return 0;
 }
+*/
+
+
+// strings as pointers
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+   char str[] = "snehal singh";
+   char* ptr = str;
+   int i = 0;
+   while(*ptr!= '\0'){
+    printf("%c",*ptr);
+    ptr++;
+    i++;
+   }
+   return 0;
+}
+
+   /* char str[] = "snehal singh";
+   char* ptr = str; //ptr now points to str[0]
+   char* ptr = &str[0];
+    printf("%p",ptr);
+    return 0;*/
+
