32c7da1814fcf732c703e8151a8693df4cdfcdcf
coderun13 <snehalsingh.0407@gmail.com>
2024-04-30 00:27:51 +0530

    gets and puts

--- c/strings.c
+++ c/strings.c
@@ -17,7 +17,7 @@
      return 0;
 };*/
 
-int main(){
+/*int main(){
     char arr[] = {"Hello Snehal\0"};
     int i = 0;
      while(arr[i]!= '\0'){
@@ -25,4 +25,22 @@ int main(){
         i++;
      }
      return 0;
-};
+};*/
+
+
+//gets(), puts() (for string)
+//%s - format specifier for string
+
+#include<string.h>
+int main(){
+    //char str[] = "snehal singh";
+    char str[40];
+
+    //scanf("%s",str); //only the first word will be considered.
+    gets(str); //entire sentence can be taken as input
+    
+    printf("the input was: %s",str);
+    //puts(str);
+
+return 0;
+}
