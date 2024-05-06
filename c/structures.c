2d82639c9dd8b4d3df8ce92e5a2a97b9332e2e85
coderun13 <snehalsingh.0407@gmail.com>
2024-05-06 21:47:46 +0530

    array of structure

--- c/structures.c
+++ c/structures.c
@@ -3,19 +3,63 @@
 
 #include<stdio.h>
 #include<string.h>
-typedef float realNumber;
+
 int main(){
+
+
+/**
+ * Array of Structure
+*/
+
+typedef struct pokemon{
+    int hp;
+    int speed;
+    int attack;
+    char tier;
+}pokemon;
+
+pokemon arr[10];
+arr[0].attack = 50;
+arr[0].hp = 150;
+arr[0].speed = 30;
+arr[0].tier = 'A';
+
+
+arr[1].attack = 60;
+arr[1].hp = 200;
+arr[1].speed = 40;
+arr[1].tier = 'B';
+
+
+arr[2].attack = 80;
+arr[2].hp = 170;
+arr[2].speed = 20;
+arr[2].tier = 'S';
+
+for(int i=0;i<3;i++){
+    printf("%d\n",arr[i].attack);
+    printf("%d\n",arr[i].hp);
+    printf("%d\n",arr[i].speed);
+    printf("%d\n",arr[i].tier);
+
+}
+
+return 0;
+
+};
+
 
 /**
  * Typedef == && the multiple pointer declaration problem (used for renaming)
  * typedef oldtype newtype; (newdatatype will be treated as old datatype)
-*/
 
+
+typedef float realNumber;
     int x;
     realNumber y = 3.1415;
     printf("%f",y);
     return 0;
-}
+}*/
 
 
 //struct person with name,age,salary
@@ -118,5 +162,3 @@ int main(){
 /**
  * Elements are stored in a continuous memory location
 */
-
-
