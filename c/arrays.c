d4404582b208827223c3d86c01bca3708582bb53
coderun13 <snehalsingh.0407@gmail.com>
2024-05-13 00:45:05 +0530

    maximum value

--- c/arrays.c
+++ c/arrays.c
@@ -1,14 +1,42 @@
 #include<stdio.h>
 int main()
+
+/**
+ * Array -> Collection
+ * Index -> start from 0
+ * Garbage value -> if value is not entered then a garbage value is stored
+*/
+
+
+//maximum value out of all elements in a array
+// {
+//    int arr[5];
+//    for(int i = 0;i<=4;i++){
+//       printf("enter the element %d ",i);
+//       scanf("%d",&arr[i]);
+//    }
+//    int max = arr[0]; //comparison by first element
+//    for(int i = 0; i<=4; i++){
+//       if(max<arr[i]){
+//       max = arr[i];
+//       }
+//    }
+//    printf("max element is %d ",max);
+// }
+
+
+//(assigning value and printing)
 // {
 //     int arr[5]= {2,4,6,8,1};
 //     arr[4]= 100;
 //     arr[1]= 1;
-//     printf("%d",arr[1]);
+//     printf("%d",arr[1]); //(assigning value)
 //     return 0;
 
 // }
 
+
+//(taking input and printing output)
 // {
 //     int arr1[4];
 //     printf("enter first number: ");
@@ -23,10 +51,12 @@ int main()
 //     printf("%d",arr1[3]);
 // }
 
-{
-     int arr[5]= {2,4,6,8,1};
 
-     for(int i = 1;i<=5;i++){
-        printf(" %d",arr[i]);
-     }
-}
+//(printing by loop)
+// {
+//      int arr[5]= {2,4,6,8,1}; 
+
+//      for(int i = 0;i<=4;i++){
+//         printf(" %d",arr[i]); 
+//      }
+// }
