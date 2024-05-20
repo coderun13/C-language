9b02b2f94999936be8218e08b0c847d5aa1f5c88
coderun13 <snehalsingh.0407@gmail.com>
2024-05-20 22:52:29 +0530

    modify function.c

--- c/functions.c
+++ c/functions.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include<math.h>
 
 //basic function
-// void greet(){
-//     printf("hello snehal\n");
-//     printf("How are you");
-//     return;
-// }
+/*void greet(){
+    printf("hello snehal\n");
+    printf("How are you");
+    return;
+}
 
-// int main() {
-//     greet();
-//     return 0;
-// }
+int main() {
+    greet();
+    return 0;
+}*/
+
 
 //add function
-int add(int a, int b){
+/*int add(int a, int b){
     return a+b;
 }
 
@@ -26,4 +28,72 @@ int main(){
     int sum = add(a,b);
     printf("%d",sum);
     return 0;
-}
+}*/
+
+
+//library function
+/* int main(){
+    int a;
+    printf("enter a number: ");
+    scanf("%d",&a);
+
+    int root = sqrt(a);
+    printf("the square root is : %d",root);
+
+    return 0;
+}*/
+
+
+//P and C
+//  int factorial(int x){
+//     int fact = 1;
+//     for (int i = 2; i <= x; i++){
+//         fact = fact*i;
+//     }
+//     return fact;
+// }
+//     int main(){
+//        int n;
+//        printf("enter n: ");
+//        scanf("%d",&n); 
+//         int r;
+//        printf("enter r: ");
+//        scanf("%d",&r); 
+        // int nfact = factorial(n);
+        // int rfact = factorial(r);
+        // int nrfact = factorial(n-r);
+
+        // int ncr = nfact/(rfact*nrfact);
+        // printf("%d",ncr);
+        // return 0;
+    // }
+
+
+//pasacal triangle
+// int factorial(int x){
+//     int fact = 1;
+//     for (int i = 2; i <= x; i++){
+//         fact = fact*i;
+//     }
+//     return fact;
+// }
+ 
+//  int combination(int n,int r){
+//     int ncr = nfact/(rfact*nrfact);
+//     return 0;
+// }
+
+// int main(){
+//     int n;
+//     printf("Enter n: ");
+//     scanf("%d",&n);
+//     int r;
+//        printf("enter r: ");
+//        scanf("%d",&r); 
+//     for(int i=0;i<=n;i++);
+//         for(int j=0;j<=i;j++);
+//         printf("*");
+//     return 0;
+
+// }
+
