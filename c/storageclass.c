191ad0acf71fe2e3f068d7dbf8dd64898d695986
coderun13 <snehalsingh.0407@gmail.com>
2024-05-23 14:47:14 +0530

    modify storage class

--- c/storageclass.c
+++ c/storageclass.c
@@ -12,6 +12,7 @@ int main()
     return 0;
 }
 
+
 //Declaration- Tellng the complier about the variable (no space reserved)
 //Definition-  declaration + space reservation
 
@@ -31,3 +32,39 @@ int main()
 */
 
 
+/* Auto storage class - 
+ * variable defined without any storage class specification.(eg- int snehal,auto int snehal are same)
+ * scope- local to the function body they sre defined to
+ * default value - garbage value
+ * lifetime- till the end of the functin block they are defined in
+*/
+
+
+/* External Storage class - 
+ * same as global variables
+ * Global variable can be changed by a function in the program
+ * scope - global to the program they are defined in
+ * default initial value: 0
+ * lfetime - declared outside the function,available throughout the lifetime of the program.
+ * Extern keyword- used to inform C complier that a given variable is declared somewhere else.
+ * Using extern will not allocate space for the variable
+ * 
+*/
+
+
+/* Static storage class- 
+ * remains under existence for use within the function for entire program run.
+ * scope- local to the block they are defined in
+ * default initial value: 0 
+ * lifetime - available throughtout the lifetime 
+ * minimize the use of unnecessary static variables in a program
+*/
+
+
+/* Register storage class -
+ * requests the complier to store the variable in the CPU register instead of storing in the memory to have faster access.
+ * Done for the variables which are being used frequently.
+ * scope - local to the function they are declared in
+ * default initial value- Garbage value
+ * lifetime - available till the end of the function block.
+*/
