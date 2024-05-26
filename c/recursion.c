1f7448e7d6660716e02b6fb9deadd3486030c03e
coderun13 <snehalsingh.0407@gmail.com>
2024-05-26 23:10:20 +0530

    add decreasing function

--- c/recursion.c
+++ c/recursion.c
@@ -23,30 +23,12 @@ int main()
 }*/
 
 
-//Print number n to 1
+//Print number n to 1(decreasing)
 
-// int factorial(int n){
-//     if(n==1 || n==0) return 1;
-//     int recAns = n*factorial(n-1);
-//     return recAns;
-// }
-
-// int main()
-// {
-//    int n;
-//    printf("enter a number: ");
-//    scanf("%d",&n);
-//    int fact = factorial(n);
-//    printf("%d",fact);
-//    return 0;
-// }
-
-//greeting 
-
-void greeting(int n){
+void decreasing(int n){
     if(n==0) return;
-    printf("Good Evening Snehal\n");
-    greeting(n-1);
+    printf("%d\n",n);
+    decreasing(n-1);
     return;
 }
 
@@ -55,10 +37,28 @@ int main()
    int n;
    printf("enter a number: ");
    scanf("%d",&n);
-   greeting(n);
+   decreasing(n);
    return 0;
 }
 
+
+//greeting 
+
+ /*void greeting(int n){
+     if(n==0) return;
+     printf("Good Evening Snehal\n");
+     greeting(n-1);
+     return;
+ }
+
+ int main()
+ {
+    int n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    greeting(n);
+    return 0;
+ }*/
 
 
 
