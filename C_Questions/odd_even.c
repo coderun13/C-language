028c9a2caefb6d4022b7ba4245392a12bd1848be
coderun13 <snehalsingh.0407@gmail.com>
2024-05-27 16:30:54 +0530

    modify odd even code

---
+++ C_Questions/odd_even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("enter a number ");
+    scanf("%d",&n);
+
+    if (n%2==0)
+    {
+        printf("the number is even");
+
+    }else{
+
+        printf("the number is odd\n");
+    }
+}    
