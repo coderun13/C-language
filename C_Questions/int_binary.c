0303c9a6c008e204729b093b1f535ae56cd93e11
coderun13 <snehalsingh.0407@gmail.com>
2024-05-27 16:28:47 +0530

    modify interger to binary conversion

---
+++ C_Questions/int_binary.c
@@ -0,0 +1,18 @@
+// #include<stdio.h>
+// void binary(int n){
+//     if(n == 0){
+//         printf("0");
+//         return;
+//     }
+//     int numBits = sizeof(n)*8;
+//     int leadingZero = 1;
+//     for (int i = numBits-1;
+//         int mask = 1 <<i ;
+//         if(n & mask){
+
+//         });
+// }
+
+
+// int main(){
+
