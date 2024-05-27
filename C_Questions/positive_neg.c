eeb315beb2c2b8d8b8a56b590e9341777ac4b378
coderun13 <snehalsingh.0407@gmail.com>
2024-05-27 16:32:37 +0530

    modify postive negative code

---
+++ C_Questions/positive_neg.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+
+int main()
+
+{
+     int n;
+    printf("enter a num: ");
+
+    scanf("%d",&n);
+
+    if(n>0){
+     
+        printf("the num is postive\n");
+
+    }else{
+
+        printf("the num is negative");
+    
+    }
+
+}
