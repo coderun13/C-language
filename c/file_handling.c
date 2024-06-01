0aabf0b9bcb93c45c6928a33f6697208a997924b
coderun13 <snehalsingh.0407@gmail.com>
2024-06-02 00:02:40 +0530

    modify file

--- c/file_handling.c
+++ c/file_handling.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
-#include<stdlib.h>
-
-/*preprocessor
- * #include ---> preprocessor directive 
- * int - 4 bytes (4*8 = 32 bits)
- * long - 8 bytes
-*/
-
-/* Macros and Macro function
- * #define PI 3.1415925359 (macros)
- * #define Area(r)(PI*r*r) (macro)
-*/
-
-/**
- * stack section ------
- *      |              |   Dynamic 
- * Free memory         |   memory 
- *      |              |   Allocation
- * Heap section -------
- * Data section -------   Static
-                       |  memory
- * Text Section -------   Allocation
-*/
-
-/* Static - the memory is allocated at the compile time.
- * Dynamic - the memory allocated at the run time.
-*/
+#include <stdlib.h>
 
 
-//malloc()
+//opening a file 
+//fopen(<filename or path>, <opening-mode>)
 int main(){
-    int* ptr = (int*)malloc(10*4);//garbage value(14686376)
-    ptr++;
-    printf("%d",*ptr);
+    FILE* ptr = fopen("c.txt","r");
+    char str[100];
+    while(fgets(str,100,ptr)!=NULL)
+        printf("%s",str);
 
-    // int* ptr = (int*)calloc(10,4); //0- garbage value
-    // ptr++;
-    // printf("%d",*ptr);
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
